9e4f3fc302302015c4133d698c2381b6f8b9857e
Lasha <lashagoglidzelasha@gmail.com>
2023-02-21 23:44:59 +0400

    Updated First Page

---
+++ main.js
@@ -0,0 +1,15 @@
+let firstID = document.querySelector("#firstID");
+let firstID2 = document.querySelector("#firstID2");
+let firstID3 = document.querySelector("#firstID3");
+let firstID4 = document.querySelector("#firstID4");
+let firstID5 = document.querySelector("#firstID5");
+let firstID6 = document.querySelector("#firstID6");
+
+let artgalery = document.querySelector("#artgalery");
+
+
+
+firstID.addEventListener("click", function () {
+
+    artgalery.style.opacity="15%";
+})
