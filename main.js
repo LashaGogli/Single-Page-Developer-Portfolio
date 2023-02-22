96448460505a9a5e0f6c4cf212a56527cdd138b7
Lasha <lashagoglidzelasha@gmail.com>
2023-02-22 13:42:01 +0400

    Active version

--- main.js
+++ main.js
@@ -6,10 +6,137 @@ let firstID5 = document.querySelector("#firstID5");
 let firstID6 = document.querySelector("#firstID6");
 
 let artgalery = document.querySelector("#artgalery");
+let artgalery2 = document.querySelector("#artgalery2");
+let artgalery3 = document.querySelector("#artgalery3");
+let artgalery4 = document.querySelector("#artgalery4");
+let artgalery5 = document.querySelector("#artgalery5");
+let artgalery6 = document.querySelector("#artgalery6");
 
 
+let viewprojectP = document.querySelector(".viewprojectP");
+let viewcode = document.querySelector(".viewcode");
+let viewprojectP2 = document.querySelector(".viewprojectP2");
+let viewcode2 = document.querySelector(".viewcode2");
+let viewprojectP3 = document.querySelector(".viewprojectP3");
+let viewcode3 = document.querySelector(".viewcode3");
+let viewprojectP4 = document.querySelector(".viewprojectP4");
+let viewcode4 = document.querySelector(".viewcode4");
+let viewprojectP5 = document.querySelector(".viewprojectP5");
+let viewcode5 = document.querySelector(".viewcode5");
+let viewprojectP6 = document.querySelector(".viewprojectP6");
+let viewcode6 = document.querySelector(".viewcode6");
 
-firstID.addEventListener("click", function () {
 
-    artgalery.style.opacity="15%";
+firstID.addEventListener("mouseover", function () {
+
+    firstID.style.position = "relative";
+    artgalery.style.position = "absolute";
+    artgalery.style.opacity = "15%";
+    viewprojectP.style.display="block";
+    viewcode.style.display="block";
+    firstID.style.cursor="pointer";
+})
+
+firstID.addEventListener("mouseleave", function () {
+
+    firstID.style.position = "none";
+    artgalery.style.position = "none";
+    artgalery.style.opacity = "100%";
+    viewprojectP.style.display="none";
+    viewcode.style.display="none";
+})
+
+firstID2.addEventListener("mouseover", function () {
+
+    firstID2.style.position = "relative";
+    artgalery2.style.position = "absolute";
+    artgalery2.style.opacity = "15%";
+    viewprojectP2.style.display="block";
+    viewcode2.style.display="block";
+    firstID2.style.cursor="pointer";
+})
+
+firstID2.addEventListener("mouseleave", function () {
+
+    firstID2.style.position = "none";
+    artgalery2.style.position = "none";
+    artgalery2.style.opacity = "100%";
+    viewprojectP2.style.display="none";
+    viewcode2.style.display="none";
+})
+
+firstID3.addEventListener("mouseover", function () {
+
+    firstID3.style.position = "relative";
+    artgalery3.style.position = "absolute";
+    artgalery3.style.opacity = "15%";
+    viewprojectP3.style.display="block";
+    viewcode3.style.display="block";
+    firstID3.style.cursor="pointer";
+})
+
+firstID3.addEventListener("mouseleave", function () {
+
+    firstID3.style.position = "none";
+    artgalery3.style.position = "none";
+    artgalery3.style.opacity = "100%";
+    viewprojectP3.style.display="none";
+    viewcode3.style.display="none";
+})
+
+firstID4.addEventListener("mouseover", function () {
+
+    firstID4.style.position = "relative";
+    artgalery4.style.position = "absolute";
+    artgalery4.style.opacity = "15%";
+    viewprojectP4.style.display="block";
+    viewcode4.style.display="block";
+    firstID4.style.cursor="pointer";
+})
+
+firstID4.addEventListener("mouseleave", function () {
+
+    firstID4.style.position = "none";
+    artgalery4.style.position = "none";
+    artgalery4.style.opacity = "100%";
+    viewprojectP4.style.display="none";
+    viewcode4.style.display="none";
+})
+
+firstID5.addEventListener("mouseover", function () {
+
+    firstID5.style.position = "relative";
+    artgalery5.style.position = "absolute";
+    artgalery5.style.opacity = "15%";
+    viewprojectP5.style.display="block";
+    viewcode5.style.display="block";
+    firstID5.style.cursor="pointer";
+})
+
+firstID5.addEventListener("mouseleave", function () {
+
+    firstID5.style.position = "none";
+    artgalery5.style.position = "none";
+    artgalery5.style.opacity = "100%";
+    viewprojectP5.style.display="none";
+    viewcode5.style.display="none";
+})
+
+firstID6.addEventListener("mouseover", function () {
+
+    firstID6.style.position = "relative";
+    artgalery6.style.position = "absolute";
+    artgalery6.style.opacity = "15%";
+    viewprojectP6.style.display="block";
+    viewcode6.style.display="block";
+    firstID6.style.cursor="pointer";
+})
+
+firstID6.addEventListener("mouseleave", function () {
+
+    firstID6.style.position = "none";
+    artgalery6.style.position = "none";
+    artgalery6.style.opacity = "100%";
+    viewprojectP6.style.display="none";
+    viewcode6.style.display="none";
 })
